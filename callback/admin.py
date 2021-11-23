00a530edcdeb5ff3863a4bb53b739c5ff69d9886
erickmamberger <erickmambergermail@yandex.ru>
2021-11-23 16:41:55 +0500

    first

--- callback/admin.py
+++ callback/admin.py
@@ -1,1 +1,18 @@
 from django.contrib import admin
+
+from callback.models import Player
+
+
+class PlayerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'created_at', 'updated_at')
+    search_fields = ('name', 'email')
+
+
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('name', 'players')
+    search_fields = ('name', 'email')
+
+
+
+admin.site.register(Player, PlayerAdmin)
+
